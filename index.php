f57df3ab2c856e146555b24e1fd2e6c043f983cd
Bathelemy Drabczuk <barthelemy.drabczuk@etu.univ-amu.fr>
2018-12-06 20:31:26 +0100

    :)

--- index.php
+++ index.php
@@ -5,12 +5,19 @@
     <link rel="stylesheet" type="text/css" href="css/cssincroyable.css"/>
 </head>
 <body>
-    <title>121 Jours en Nabim</title>
+    <h1>121 Jours en Nabim</h1>
     <nav id="menu">
         <a>La mission</a>
         <a>Postuler</a>
         <a>Description</a>
         <a>Connexion</a>
     </nav>
+    <p>Super le blabla j'adore , voila notre site il est trop bien wallah</p>
+    <div id="checklist">
+        <input type="checkbox"/>Bien penser à se brosser les dents<br/>
+        <input type="checkbox"/>Salut a tous j'ai la flemme de réfléchir<br/>
+        <input type="checkbox"/>Deborah veut pas faire de css lul<br/>
+        <input type="checkbox"/>Bon 4 ça suffira ?<br/>
+    </div>
 </body>
 </html>
