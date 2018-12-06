fc7650a12ce14175285d4fc0562893f614cc4162
Bathelemy Drabczuk <barthelemy.drabczuk@etu.univ-amu.fr>
2018-12-06 20:15:44 +0100

    :)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/cssincroyable.css"/>
+</head>
+<body>
+    <title>121 Jours en Nabim</title>
+    <nav id="menu">
+        <a>La mission</a>
+        <a>Postuler</a>
+        <a>Description</a>
+        <a>Connexion</a>
+    </nav>
+</body>
+</html>
