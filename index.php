099ba7c0b8f59f7159d6dc737747c4c54d118939
Barthélémy Drabczuk <barthelemy.drabczuk@etu.univ-amu.fr>
2018-12-07 02:32:35 +0100

    ajout de la fenêtre

--- index.php
+++ index.php
@@ -7,6 +7,31 @@
     <script src="src/js/fusee.js"></script>
     <script src="src/js/konomi.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            var $cpt = 0;
+            $('#chatBox').hide ();
+            $('#close').hide();
+            $('#theFlyingDiv').click(function(){
+                if ($cpt === 0) {
+                    $('#close').show();
+                    $('#chatBox').show();
+                    $('#theFlyingDiv').css('padding-left', '1%');
+                    $('#theFlyingDiv').css('padding-right', '1%');
+                    ++$cpt;
+                }
+                else {
+                    $cpt = 0;
+                }
+            });
+            $('#close').click(function(){
+                $('#chatBox').hide();
+                $('#close').hide();
+                $('#theFlyingDiv').css('padding-left', '7%');
+                $('#theFlyingDiv').css('padding-right', '7%');
+            });
+        });
+    </script>
 </head>
 <body>
 <div class="header">
@@ -37,9 +62,19 @@
     </table>
 
 
-    <div id="theFlyingDiv">
+<div id="theFlyingDiv">
+    <div id="pourFaireJoli">
         assistance
+        <p id="close">x</p>
     </div>
+    <div id="chatBox">
+        Ut ullamcorper blandit mi, at egestas dui vehicula vitae. Nulla ullamcorper semper orci, vel finibus mi. Fusce lobortis dui in tortor hendrerit vulputate. Aenean eleifend efficitur enim, nec ultrices ante mattis vitae. Curabitur nec massa nisi. Curabitur vestibulum, nibh nec mollis venenatis, mi turpis elementum quam, convallis aliquam nunc magna eu turpis. Donec maximus id mi sed tincidunt. Duis elementum ultricies ante nec volutpat. Cras eget vehicula urna. Aliquam nec sapien nibh. Cras dapibus magna sed luctus varius. Nullam eleifend suscipit nisl. Fusce in elit tincidunt, vestibulum quam vel, laoreet ipsum. Pellentesque non posuere eros. Sed non ligula metus. Integer blandit augue lectus, non cursus diam vestibulum eleifend.
+        <div id="formDeValid">
+            <input type="text" id="chat"/>
+            <input type="submit"/>
+        </div>
+    </div>
+</div>
     <img src="https://image.noelshack.com/fichiers/2018/49/5/1544143798-fusee.png" id="fusee" alt="fusee" style="display:none"/>
 
 </body>
