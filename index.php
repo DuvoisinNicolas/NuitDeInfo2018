e5f58199905bcdc82f361dc9a2bcb7af81d80f1a
un bel homme <mathias.meyer.isen@gmail.com>
2018-12-07 02:14:59 +0100

    Merge branch 'master' of D:\NuitDeInfo2018 with conflicts.

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" type="text/css" href="css/cssincroyable.css"/>
-    <link rel="icon" href="https://image.noelshack.com/fichiers/2018/49/5/1544143799-crewstillant.jpg" type="image/jpg" sizes="16x16">
+    <link rel="icon" href="src/resources/crewstillant.jpg" type="image/jpg" sizes="16x16">
     <script src="src/js/fusee.js"></script>
     <script src="src/js/konomi.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
 <div class="header">
-    <img src="https://image.noelshack.com/fichiers/2018/49/5/1544143798-namib.jpg" alt="Va mourir !">
+    <img src="/src/resources/namib.jpg" alt="Va mourir !">
 </div>
     <h2 id="titre">121 Jours sur Namib</h2>
     <ul>
@@ -20,27 +20,17 @@
         <li><a>Connexion</a></li>
     </ul>
     <p>Salut voilà le super site</p>
-    <table>
-        <td class = "checklistc">
-            <div id="checklist">
-                <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Bien penser à se brosser les dents<br/>
-                <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Salut a tous j'ai la flemme de réfléchir<br/>
-                <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Deborah veut pas faire de css lul<br/>
-                <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Bon 4 ça suffira ?<br/>
-            </div>
-        </td>
-
-        <td>
-            <p id="rightText">LOREM IPSUM LOL :D</p>
-        </td>
-
-    </table>
-
-
+    <div id="checklist">
+        <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Bien penser à se brosser les dents<br/>
+        <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Salut a tous j'ai la flemme de réfléchir<br/>
+        <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Deborah veut pas faire de css lul<br/>
+        <input type="checkbox" class="checkboxJS" onchange="checkBox(this)"/>Bon 4 ça suffira ?<br/>
+    </div>
+    <p id="rightText">LOREM IPSUM LOL :D</p>
     <div id="theFlyingDiv">
         assistance
     </div>
-    <img src="https://image.noelshack.com/fichiers/2018/49/5/1544143798-fusee.png" id="fusee" alt="fusee" style="display:none"/>
+    <img src="src/resources/fusee.png" id="fusee" alt="fusee" style="display:none"/>
 
 </body>
 </html>
